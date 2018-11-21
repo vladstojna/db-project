cdf08a28e7fe45dd18190635f49c6c172a299014
TricksterByte <psilvacenteno@gmail.com>
2018-11-21 15:16:24 +0000

    added cascade dropping, parentheses on keys

--- schema.sql
+++ schema.sql
@@ -1,28 +1,28 @@
 /* database schema */
 
-DROP TABLE camera;
-DROP TABLE video;
-DROP TABLE video_segment;
+DROP TABLE IF EXISTS camera CASCADE;
+DROP TABLE IF EXISTS  video CASCADE;
+DROP TABLE IF EXISTS video_segment CASCADE;;
 
-DROP TABLE place;
-DROP TABLE lookout;
+DROP TABLE IF EXISTS place CASCADE;
+DROP TABLE IF EXISTS lookout CASCADE;
 
-DROP TABLE emergency_event;
-DROP TABLE rescue_process;
+DROP TABLE IF EXISTS emergency_event CASCADE;;
+DROP TABLE IF EXISTS rescue_process CASCADE;
 
-DROP TABLE medium_entity;
-DROP TABLE medium;
-DROP TABLE medium_combat;
-DROP TABLE medium_support;
-DROP TABLE medium_rescue;
+DROP TABLE IF EXISTS medium_entity CASCADE;
+DROP TABLE IF EXISTS medium CASCADE;
+DROP TABLE IF EXISTS medium_combat CASCADE;
+DROP TABLE IF EXISTS medium_support CASCADE;
+DROP TABLE IF EXISTS medium_rescue CASCADE;
 
-DROP TABLE transports;
-DROP TABLE allocated;
-DROP TABLE triggers;
+DROP TABLE IF EXISTS transports CASCADE;
+DROP TABLE IF EXISTS allocated CASCADE;
+DROP TABLE IF EXISTS triggers CASCADE;
 
-DROP TABLE coordinator;
-DROP TABLE audits;
-DROP TABLE requests;
+DROP TABLE IF EXISTS coordinator CASCADE;
+DROP TABLE IF EXISTS audits CASCADE;
+DROP TABLE IF EXISTS requests CASCADE;
 
 /* Table creation */
 
@@ -31,7 +31,7 @@ DROP TABLE requests;
 CREATE TABLE camera(
 	camera_id INTEGER NOT NULL,
 
-	PRIMARY KEY camera_id
+	PRIMARY KEY(camera_id)
 );
 
 CREATE TABLE video(
@@ -39,8 +39,8 @@ CREATE TABLE video(
 	date_time_end   TIMESTAMP(3) NOT NULL,
 	camera_id       INTEGER      NOT NULL,
 
-	PRIMARY KEY (date_time_start, camera_id),
-	FOREIGN KEY camera_id REFERENCES camera(camera_id)
+	PRIMARY KEY(date_time_start, camera_id),
+	FOREIGN KEY(camera_id) REFERENCES camera(camera_id)
 );
 
 CREATE TABLE video_segment(
@@ -49,8 +49,8 @@ CREATE TABLE video_segment(
 	date_time_start TIMESTAMP(3) NOT NULL,
 	camera_id       INTEGER      NOT NULL,
 
-	PRIMARY KEY (segment_number, date_time_start, camera_id),
-	FOREIGN KEY (date_time_start, camera_id) REFERENCES video(date_time_start, camera_id)
+	PRIMARY KEY(segment_number, date_time_start, camera_id),
+	FOREIGN KEY(date_time_start, camera_id) REFERENCES video(date_time_start, camera_id)
 );
 
 --------------------------------------------------------------------------
@@ -65,12 +65,18 @@ CREATE TABLE lookout(
 	place_address VARCHAR(100) NOT NULL,
 	camera_id     INTEGER      NOT NULL,
 
-	PRIMARY KEY (place_address, camera_id),
-	FOREIGN KEY place_address REFERENCES place(place_address),
-	FOREIGN KEY camera_id     REFERENCES camera(camera_id)
+	PRIMARY KEY(place_address, camera_id),
+	FOREIGN KEY(place_address) REFERENCES place(place_address),
+	FOREIGN KEY(camera_id)     REFERENCES camera(camera_id)
 );
 
 --------------------------------------------------------------------------
+
+CREATE TABLE rescue_process(
+	rescue_process_number INTEGER NOT NULL,
+
+	PRIMARY KEY(rescue_process_number)
+);
 
 CREATE TABLE emergency_event(
 	phone_number          NUMERIC(9, 0) NOT NULL,
@@ -79,15 +85,9 @@ CREATE TABLE emergency_event(
 	place_address         VARCHAR(255)  NOT NULL,
 	rescue_process_number INTEGER,
 
-	PRIMARY KEY (phone_number, call_time),
-	FOREIGN KEY rescue_process_number REFERENCES rescue_process(rescue_process_number),
-	UNIQUE (phone_number, person_name)
-);
-
-CREATE TABLE rescue_process(
-	rescue_process_number INTEGER NOT NULL,
-
-	PRIMARY KEY rescue_process_number
+	PRIMARY KEY(phone_number, call_time),
+	FOREIGN KEY(rescue_process_number) REFERENCES rescue_process(rescue_process_number),
+	UNIQUE(phone_number, person_name)
 );
 
 --------------------------------------------------------------------------
@@ -95,7 +95,7 @@ CREATE TABLE rescue_process(
 CREATE TABLE medium_entity(
 	entity_name VARCHAR(80) NOT NULL,
 
-	PRIMARY KEY entity_name
+	PRIMARY KEY(entity_name)
 );
 
 CREATE TABLE medium(
@@ -103,32 +103,32 @@ CREATE TABLE medium(
 	medium_name   VARCHAR(80) NOT NULL,
 	entity_name   VARCHAR(80) NOT NULL,
 
-	PRIMARY KEY (medium_number, entity_name),
-	FOREIGN KEY entity_name REFERENCES medium_entity(entity_name)
+	PRIMARY KEY(medium_number, entity_name),
+	FOREIGN KEY(entity_name) REFERENCES medium_entity(entity_name)
 );
 
 CREATE TABLE medium_combat(
 	medium_number INTEGER     NOT NULL,
 	entity_name   VARCHAR(80) NOT NULL,
 
-	PRIMARY KEY (medium_number, entity_name),
-	FOREIGN KEY (medium_number, entity_name) REFERENCES medium(medium_number, entity_name)
+	PRIMARY KEY(medium_number, entity_name),
+	FOREIGN KEY(medium_number, entity_name) REFERENCES medium(medium_number, entity_name)
 );
 
 CREATE TABLE medium_support(
 	medium_number INTEGER     NOT NULL,
 	entity_name   VARCHAR(80) NOT NULL,
 
-	PRIMARY KEY (medium_number, entity_name),
-	FOREIGN KEY (medium_number, entity_name) REFERENCES medium(medium_number, entity_name)
+	PRIMARY KEY(medium_number, entity_name),
+	FOREIGN KEY(medium_number, entity_name) REFERENCES medium(medium_number, entity_name)
 );
 
 CREATE TABLE medium_rescue(
 	medium_number INTEGER     NOT NULL,
 	entity_name   VARCHAR(80) NOT NULL,
 
-	PRIMARY KEY (medium_number, entity_name),
-	FOREIGN KEY (medium_number, entity_name) REFERENCES medium(medium_number, entity_name)
+	PRIMARY KEY(medium_number, entity_name),
+	FOREIGN KEY(medium_number, entity_name) REFERENCES medium(medium_number, entity_name)
 );
 
 --------------------------------------------------------------------------
@@ -139,9 +139,9 @@ CREATE TABLE transports(
 	victim_number         INTEGER     NOT NULL,
 	rescue_process_number INTEGER     NOT NULL,
 
-	PRIMARY KEY (medium_number, entity_name, rescue_process_number),
-	FOREIGN KEY (medium_number, entity_name) REFERENCES medium_rescue(medium_number, entity_name),
-	FOREIGN KEY rescue_process_number REFERENCES rescue_process(rescue_process_number)
+	PRIMARY KEY(medium_number, entity_name, rescue_process_number),
+	FOREIGN KEY(medium_number, entity_name) REFERENCES medium_rescue(medium_number, entity_name),
+	FOREIGN KEY(rescue_process_number) REFERENCES rescue_process(rescue_process_number)
 );
 
 CREATE TABLE allocated(
@@ -150,9 +150,9 @@ CREATE TABLE allocated(
 	hours_done            INTEGER     NOT NULL,
 	rescue_process_number INTEGER     NOT NULL,
 
-	PRIMARY KEY (medium_number, entity_name, rescue_process_number),
-	FOREIGN KEY (medium_number, entity_name) REFERENCES medium_support(medium_number, entity_name),
-	FOREIGN KEY rescue_process_number REFERENCES rescue_process(rescue_process_number)
+	PRIMARY KEY(medium_number, entity_name, rescue_process_number),
+	FOREIGN KEY(medium_number, entity_name) REFERENCES medium_support(medium_number, entity_name),
+	FOREIGN KEY(rescue_process_number) REFERENCES rescue_process(rescue_process_number)
 );
 
 CREATE TABLE triggers(
@@ -160,9 +160,9 @@ CREATE TABLE triggers(
 	entity_name           VARCHAR(80) NOT NULL,
 	rescue_process_number INTEGER     NOT NULL,
 
-	PRIMARY KEY (medium_number, entity_name, rescue_process_number),
-	FOREIGN KEY (medium_number, entity_name) REFERENCES medium(medium_number, entity_name),
-	FOREIGN KEY rescue_process_number REFERENCES rescue_process(rescue_process_number)
+	PRIMARY KEY(medium_number, entity_name, rescue_process_number),
+	FOREIGN KEY(medium_number, entity_name) REFERENCES medium(medium_number, entity_name),
+	FOREIGN KEY(rescue_process_number) REFERENCES rescue_process(rescue_process_number)
 );
 
 --------------------------------------------------------------------------
@@ -170,7 +170,7 @@ CREATE TABLE triggers(
 CREATE TABLE coordinator(
 	coordinator_id INTEGER NOT NULL,
 
-	PRIMARY KEY coordinator_id
+	PRIMARY KEY(coordinator_id)
 );
 
 CREATE TABLE audits(
@@ -183,10 +183,10 @@ CREATE TABLE audits(
 	audition_date         DATE         NOT NULL,
 	text                  TEXT         NOT NULL,
 
-	PRIMARY KEY (coordinator_id, medium_number, entity_name, rescue_process_number),
-	FOREIGN KEY (medium_number, entity_name, rescue_process_number)
+	PRIMARY KEY(coordinator_id, medium_number, entity_name, rescue_process_number),
+	FOREIGN KEY(medium_number, entity_name, rescue_process_number)
 		REFERENCES triggers(medium_number, entity_name, rescue_process_number),
-	FOREIGN KEY coordinator_id REFERENCES coordinator(coordinator_id)
+	FOREIGN KEY(coordinator_id) REFERENCES coordinator(coordinator_id)
 );
 
 CREATE TABLE requests(
@@ -196,7 +196,7 @@ CREATE TABLE requests(
 	date_time_start       TIMESTAMP(3) NOT NULL,
 	date_time_end         TIMESTAMP(3) NOT NULL,
 
-	PRIMARY KEY (coordinator_id, video_date_time_start, camera_id),
-	FOREIGN KEY coordinator_id REFERENCES coordinator(coordinator_id),
-	FOREIGN KEY (video_date_time_start, camera_id) REFERENCES video(date_time_start, camera_id)
+	PRIMARY KEY(coordinator_id, video_date_time_start, camera_id),
+	FOREIGN KEY(coordinator_id) REFERENCES coordinator(coordinator_id),
+	FOREIGN KEY(video_date_time_start, camera_id) REFERENCES video(date_time_start, camera_id)
 );
