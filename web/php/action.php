7d266c80ace7a93caced587e580dfd2bd91ed72e
vladstojna <vlad.leic@gmail.com>
2018-11-24 18:17:18 +0000

    action mapper

---
+++ web/php/action.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+	</head>
+	<body>
+<?php
+	try {
+		include 'config.php';
+
+		$table  = $_REQUEST['table'];
+		$action = $_REQUEST['action'];
+
+		switch ($action) {
+			/* In case of insertion */
+			case 'insert':
+				include 'insert.php';
+				break;
+			/* In case of deletion */
+			case 'delete':
+				include 'delete.php';
+				break;
+			/* In case of edit */
+			case 'edit':
+				include 'update.php';
+				break;
+			/* When associating rescue processes */
+			case 'associate':
+				include 'associate.php';
+				break;
+			/* When listing a relation */
+			case 'list':
+				include 'list.php';
+				break;
+		}
+
+		echo("<a href=\"../html/index_a.html\">~ Go back</a>");
+
+		$db = null;
+		
+	}
+	catch (PDOException $e) {
+		echo("<p>ERROR: {$e->getMessage()}</p>");
+	}
+?>
+	</body>
+</html>
+
