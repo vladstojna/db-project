fffeeed25310699c39277bd58c2958601fc70a43
vladstojna <vlad.leic@gmail.com>
2018-11-25 15:10:57 +0000

    simplified code

--- web/php/list.php
+++ web/php/list.php
@@ -5,28 +5,25 @@
 
 		$table = $_GET['table'];
 
+		$sql = "SELECT * FROM $table";
+
 		switch ($table) {
 
 			case 'rescue_process':
 
-				$sql = "SELECT rescue_process_number FROM rescue_process;";
 				print_table($db, $sql, "Rescue Processes", ["rescue_process_number"]);
-				echo("<a href=\"../html/index_c.html\">~ Go back</a>");
-
 				break;
 
 			case 'medium':
 
-				$columns = "medium_number,medium_name,entity_name";
-				$sql = "SELECT $columns FROM medium;";
-				print_table($db, $sql, "Mediums", explode(",", $columns));
-				echo("<a href=\"../html/index_c.html\">~ Go back</a>");
-
+				print_table($db, $sql, "Mediums", ["medium_number", "medium_name", "entity_name"]);
 				break;
 		}
 
+		print_link("../index.html", "~ Go back");
+
 		$db = null;
-		
+
 	}
 	catch (PDOException $e) {
 		echo("<p>ERROR: {$e->getMessage()}</p>");
