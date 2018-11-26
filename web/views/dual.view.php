8255d578b65e9c7d6ee993cbb27132568f0d2c97
vladstojna <vlad.leic@gmail.com>
2018-11-26 17:15:55 +0000

    added generic views

---
+++ web/views/dual.view.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../../css/style.css">
+</head>
+<body>
+
+<a href="../../index.html"> ~ Back </a>
+
+<?php table($helper) ?>
+
+<?php table($result) ?>
+
+</body>
+</html>
+
---
+++ web/views/simple.view.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../../css/style.css">
+</head>
+<body>
+
+<a href="../../index.html"> ~ Back </a>
+
+<?php table($table) ?>
+
+</body>
+</html>
+
