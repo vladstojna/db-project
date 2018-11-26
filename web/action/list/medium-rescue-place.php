78a59b6b41ad4fd79ef53df2275d8641f807006c
vladstojna <vlad.leic@gmail.com>
2018-11-26 17:36:20 +0000

    filtered listings done

---
+++ web/action/list/medium-rescue-place.php
@@ -0,0 +1,31 @@
+<?php
+
+require '../../common/init.php';
+
+if (isset($_GET['place_address'])) {
+
+	$sql =
+		"SELECT DISTINCT medium_number, medium_name, entity_name
+		FROM emergency_event
+			NATURAL INNER JOIN triggers
+			NATURAL INNER JOIN medium_rescue
+			NATURAL INNER JOIN medium
+		WHERE place_address = :address;";
+
+	$result = prepare($sql);
+	$result->bindParam(':address', $_GET['place_address']);
+	$result->execute();
+
+	$result = table_params($result, "Triggered Rescue Mediums",
+		["medium_number", "medium_name", "entity_name"]
+	);
+
+}
+
+$sql = "SELECT DISTINCT place_address
+		FROM emergency_event NATURAL INNER JOIN triggers;";
+
+$helper = table_params(query($sql), "Places", ["place_address"], ["place_address"]);
+
+include '../../views/dual.view.php';
+
