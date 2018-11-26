42c5d911153cf2e3e21b5f3035dad34200c00a72
vladstojna <vlad.leic@gmail.com>
2018-11-26 14:12:50 +0000

    renamed & new function

--- web/common/helpers.php
+++ web/common/helpers.php
@@ -1,6 +1,10 @@
-<?
+<?php
 
-function view($file) {
+function include_view($file) {
 	include __DIR__.'/../views/'.$file;
+}
+
+function include_table($file) {
+	include __DIR__.'/../tables/'.$file;
 }
 
