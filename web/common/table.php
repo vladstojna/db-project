13b8ad869cc32141c4b9811975146c385778166e
vladstojna <vlad.leic@gmail.com>
2018-11-26 17:20:17 +0000

    added new table param function

--- web/common/table.php
+++ web/common/table.php
@@ -1,24 +1,34 @@
 <?php
 
-function table($result, $title, $columns, $form=null) {
-	if (isset($result) && isset($columns) && isset($title)) { ?>
-	<h3> <?=$title?> </h3>
+function table_params($result, $title, $columns, $form=null) {
+	return array(
+		'r'=>$result,
+		't'=>$title,
+		'c'=>$columns,
+		'f'=>$form
+	);
+}
+
+function table($arg) {
+	if (isset($arg['r']) && isset($arg['c']) && isset($arg['t'])) { ?>
+	<h3> <?=$arg['t']?> </h3>
 	<table border=1>
 		<tr>
-		<?php foreach($columns as $col) { ?>
+		<?php foreach($arg['c'] as $col) { ?>
 			<th> <?=$col?> </th>
 		<?php } ?>
 		</tr>
-		<?php foreach($result as $row) { ?>
+		<?php foreach($arg['r'] as $row) { ?>
 		<tr>
-			<?php foreach($columns as $col) { ?>
+			<?php foreach($arg['c'] as $col) { ?>
 				<td> <?=$row[$col]?> </td>
 			<?php } ?>
-			<?php if (isset($form)) { ?>
+			<?php if (isset($arg['f'])) { ?>
 				<td>
 				<form action="" method="GET">
-					<?php foreach($form as $in) ?>
+					<?php foreach($arg['f'] as $in) { ?>
 						<input type="hidden" name="<?=$in?>" value="<?=$row[$in]?>"/>
+					<?php } ?>
 					<input type="submit" value="Submit"/>
 				</form>
 				</td>
