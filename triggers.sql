d43a0726b91afc31d815da9f2d9e7dfe4542b7ba
vladstojna <vlad.leic@gmail.com>
2018-11-27 16:24:40 +0000

    added rescue process number validity trigger

--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,6 @@
+
+/* autoFill: implements auto incrementing id to video segments per video */
+
 CREATE OR REPLACE FUNCTION autoFill() RETURNS trigger AS $$
 DECLARE x INTEGER;
 BEGIN
@@ -11,4 +14,29 @@ BEGIN
 
 END $$ LANGUAGE plpgsql;
 
+
+/* processValidity: does not allow more processes than emergency events */
+
+CREATE OR REPLACE FUNCTION processValidity() RETURNS trigger AS $$
+DECLARE p INTEGER;
+DECLARE e INTEGER;
+BEGIN
+
+	p := COUNT(*) FROM rescue_process;
+	e := COUNT(*) FROM emergency_event;
+
+	IF p >= e THEN
+		RAISE EXCEPTION 'There cannot be more Rescue Processes than Emergency Events';
+	END IF;
+
+	RETURN NEW;
+
+END $$ LANGUAGE plpgsql;
+
+
+/* Create triggers */
+
 CREATE TRIGGER autoFillSeg BEFORE INSERT ON video_segment FOR EACH ROW EXECUTE PROCEDURE autoFill();
+
+CREATE TRIGGER validateProc BEFORE INSERT ON rescue_process FOR EACH ROW EXECUTE PROCEDURE processValidity();
+
