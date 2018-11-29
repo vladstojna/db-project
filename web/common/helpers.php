8487a7783b75eb96af5b3e2498348b539153b154
vladstojna <vlad.leic@gmail.com>
2018-11-29 18:17:38 +0000

    added template function

--- web/common/helpers.php
+++ web/common/helpers.php
@@ -6,3 +6,17 @@ function view($file) {
 	return ROOT_DIR . '/views/' . $file;
 }
 
+function template($file, $args) {
+	$path = ROOT_DIR . '/views/' . $file . '.php';
+	
+	if (!file_exists($path))
+		return null;
+
+	if (is_array($args))
+		extract($args);
+
+	ob_start();
+		include $path;
+	return ob_get_clean();
+}
+
