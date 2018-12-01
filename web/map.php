7fb1b83a8ed73f4fd75cdcd6981f3791ba3abee3
vladstojna <vlad.leic@gmail.com>
2018-12-01 18:06:27 +0000

    increased mapper security

--- web/map.php
+++ web/map.php
@@ -4,20 +4,45 @@
 
 /* get data sent by user */
 
-$action = $_REQUEST['action'];
-$data   = $_REQUEST['data'];
+$action = $_POST['action'];
+$data   = $_POST['data'];
 
 if (isset($action) && !empty($action) && isset($data) && !empty($data)):
 
-	header('Location: action/'.$action.'/'. $data.'.php');
+	$all_data = array(
+		'place',
+		'emergency-event',
+		'rescue-process',
+		'medium',
+		'entity',
+		'medium-combat',
+		'medium-rescue',
+		'medium-support');
 
-else: ?>
+	$updatable_data = array(
+		'medium-combat',
+		'medium-rescue',
+		'medium-support');
+
+	$valid = false;
+
+	if (($action == 'insert' || $action == 'delete') && $action != 'update'):
+		$valid = in_array($data, $all_data);
+	else:
+		$valid = in_array($data, $updatable_data);
+	endif;
+
+	if ($valid):
+		header('Location: action/'.$action.'/'. $data.'.php');
+	endif;
+
+endif;
+
+?>
 
 <html>
 <body>
 	<h1> Nothing to do here <h1>
 </body
 </html>
-
-<?php endif ?>
 
