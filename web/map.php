c2ab897c3d5519ab3ecd8bce25e10e407a2ab1c5
vladstojna <vlad.leic@gmail.com>
2018-12-01 17:37:03 +0000

    redone using forms & mapper

--- web/map.php
+++ web/map.php
@@ -2,14 +2,22 @@
 
 /* php mapper: from index to particular script */
 
-require_once 'common/init.php';
-
 /* get data sent by user */
 
-if (isset($_REQUEST['action']) && isset($_REQUEST['data'])) {
-	$action = $_REQUEST['action'];
-	$data   = $_REQUEST['data'];
-}
+$action = $_REQUEST['action'];
+$data   = $_REQUEST['data'];
 
-require_once ROOT_DIR . '/action/' . $action . '/' . $data . '.php';
+if (isset($action) && !empty($action) && isset($data) && !empty($data)):
+
+	header('Location: action/'.$action.'/'. $data.'.php');
+
+else: ?>
+
+<html>
+<body>
+	<h1> Nothing to do here <h1>
+</body
+</html>
+
+<?php endif ?>
 
