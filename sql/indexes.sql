2402904f84b17e345308c05f6d33c063005d4eb8
vladstojna <vlad.leic@gmail.com>
2018-12-11 19:20:05 +0000

    redone some indexes

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -5,8 +5,12 @@ DROP INDEX IF EXISTS index_lout_cam_place;
 DROP INDEX IF EXISTS index_transports_rpn;
 DROP INDEX IF EXISTS index_event_rpn;
 
-CREATE INDEX index_vid_cam ON video USING hash (camera_id);
+
+CREATE INDEX index_vid_cam ON video USING btree (camera_id);
+CLUSTER video USING index_vid_cam;
+
 CREATE UNIQUE INDEX index_lout_cam_place ON lookout USING btree (place_address, camera_id);
+
 
 CREATE INDEX index_transports_rpn ON transports USING hash (rescue_process_number);
 CREATE INDEX index_event_rpn ON emergency_event USING hash (rescue_process_number);
